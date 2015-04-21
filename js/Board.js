37b8bd8ac5906ef1af2451490398f0d6b368c506
Taylor Premo <tain.premo@gmail.com>
2015-04-21 14:01:49 -0500

    Initial commit, started rewriting to be structured better.

---
+++ js/Board.js
@@ -0,0 +1,17 @@
+//Taylor Premo - 2048
+//Game.js
+
+/**
+ * Handles management of board mechanics
+ * and moving pieces on the board.
+ */
+
+var Board = function (size) {
+    this.size = size;
+    var pieces = [[]];
+
+    var slideUp = function(){};
+    var slideRight = function(){};
+    var slideDown = function(){};
+    var slideLeft = function(){};
+};
